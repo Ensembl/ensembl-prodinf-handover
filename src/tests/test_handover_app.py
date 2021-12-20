cb2431f710e88d83add31a401416d47ed1ddd140
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2021-12-20 15:55:57 +0000

    Fix build attempt 2

--- src/tests/test_handover_app.py
+++ src/tests/test_handover_app.py
@@ -1,43 +1,24 @@
-import unittest
+# See the NOTICE file distributed with this work for additional information
+#   regarding copyright ownership.
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#       http://www.apache.org/licenses/LICENSE-2.0
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 
-import pytest
-import requests
-from requests.exceptions import ConnectionError
+import unittest
 
 from ensembl.production.handover.app.main import valid_handover
 from ensembl.production.handover.celery_app import utils as ut
 from ensembl.production.handover.celery_app.utils import parse_db_infos
 
 
-def is_responsive(url):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            return True
-    except ConnectionError:
-        return False
-
-
-@pytest.fixture(scope="session")
-def http_service(docker_ip, docker_services):
-    """Ensure that HTTP service is up and responsive."""
-
-    # `port_for` takes a container port and returns the corresponding host port
-    port = docker_services.port_for("handover_app", 5000)
-    url = "http://{}:{}".format(docker_ip, port)
-    docker_services.wait_until_responsive(
-        timeout=30.0, pause=0.1, check=lambda: is_responsive(url)
-    )
-    return url
-
-
-def test_status_code(http_service):
-    status = 200
-    response = requests.get(http_service)
-    assert response.status_code == status
-
-
 class TestHandover(unittest.TestCase):
+
     def test_valid_handover_valid(self):
         release = '101'
         valid_uris = [
