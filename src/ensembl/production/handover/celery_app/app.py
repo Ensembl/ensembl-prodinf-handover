be42d55f65cf49a75ec0d82d458bf9dc90aeec87
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2023-05-09 19:31:35 +0100

    Another attempt to fix Celery config

--- src/ensembl/production/handover/celery_app/app.py
+++ src/ensembl/production/handover/celery_app/app.py
@@ -15,13 +15,11 @@ import logging
 
 from celery import Celery
 
-app = Celery('ensembl_handover_celery',
-             include=['tasks'])
-
-# Load the externalised config module from PYTHONPATH
 try:
-    from ensembl.production.handover.config import HandoverCeleryConfig 
-
+    from ensembl.production.handover.config import HandoverCeleryConfig
+    import ensembl.production.handover.celery_app.tasks
+    app = Celery('ensembl_handover_celery',
+                 include=['ensembl.production.handover.celery_app.tasks'])
     app.config_from_object(HandoverCeleryConfig)
 except Exception as e:
     print(e)
